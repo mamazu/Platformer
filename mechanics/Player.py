6ec6793a4dcbd7a4b49cb4ae9fbf531a4adb329e
mamazu <maxo.pech@gmail.com>
2017-03-24 21:27:03 +0100

    Implemented sprite flipping on player change direction

--- mechanics/Player.py
+++ mechanics/Player.py
@@ -11,11 +11,20 @@ class Player(Entity):
         self.size = Vec2D(30, 30) if not isinstance(size, int) else Vec2D(abs(size), abs(size))
         self.set_image('res/doodle.png')
         self.movement = Vec2D(Player.DEFAULT_SPEED, Player.DEFAULT_GRAVITY)
+        self.control(1)
         self.health = 100
         self.jumping = 0
 
     def control(self, x):
-        self.movement.x = 0 if x == 0 else x / abs(x) * Player.DEFAULT_SPEED
+        if x == 0:
+            self.movement.x = 0
+            return
+        direction = x / abs(x)
+        # Flipping the image of the player if he turns around
+        if direction != self.movement / Player.DEFAULT_SPEED:
+            from pygame.transform import flip
+            self.image = flip(self.image, True, False)
+        self.movement.x = direction * Player.DEFAULT_SPEED
 
     def move(self):
         Entity.move(self)
